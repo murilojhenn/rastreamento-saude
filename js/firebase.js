bee6b72c8ee3efa8069d604c1dc9e2dedf418c6e
murilojhenn <passosmiguel653@gmail.com>
2026-06-17 20:54:44 -0400

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,48 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    getDoc,
+    updateDoc,
+    deleteDoc,
+    doc,
+    query,
+    where,
+    orderBy,
+    limit,
+    onSnapshot,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBcG7UwJS7Fchdhfv7SbgP59AXWlwkmWwE",
+    authDomain: "rastreamento-saude.firebaseapp.com",
+    projectId: "rastreamento-saude",
+    storageBucket: "rastreamento-saude.firebasestorage.app",
+    messagingSenderId: "630037928416",
+    appId: "1:630037928416:web:616ef55c9e1ca251507385"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export {
+    db,
+    collection,
+    addDoc,
+    getDocs,
+    getDoc,
+    updateDoc,
+    deleteDoc,
+    doc,
+    query,
+    where,
+    orderBy,
+    limit,
+    onSnapshot,
+    serverTimestamp
+};
